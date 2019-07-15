797d6a257824e90b384853acaf6ced7eeef6ad44
Roberto Pereira DeDeus Jr <robertopereiradedeusjr@Johnny5.home>
2019-07-15 19:44:41 -0400

    Display app-heros.

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
-  styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
+
+  hero = "Windstorm";
 
   constructor() { }
 
