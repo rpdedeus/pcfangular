5d70f6b9fa3540305741a40494e45e7810caa827
Roberto DeDeus Jr <roberto.p.dedeus@gmail.com>
2019-07-16 11:11:30 -0400

    Remove mock heros. Use http client for get hero.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { MessageService } from './message.service';
-import { HEROES } from './mock-heros';
 import { Hero } from './hero';
 
 @Injectable({
@@ -18,19 +18,37 @@ export class HeroService {
     private messageService: MessageService
     ) { }
 
-  getHero(id: number) {
-    // TODO: send message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap( _ => this.log(`fetched hero id = ${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   getHeroes(): Observable<Hero[]> {
-    // TODO: send message _after_ fetching the heroes
-    this.messageService.add("HeroService: fetched heroes");
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error, any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consuption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    }
   }
 }
