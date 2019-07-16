6f913d22a692c2d90727429fc7e9aaf3222d7281
Roberto Pereira DeDeus Jr <robertopereiradedeusjr@Johnny5.home>
2019-07-15 22:42:04 -0400

    Add route for heroes component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HerosComponent } from './heros/heros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HerosComponent}
+];  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
