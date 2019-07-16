730e5248d4ea46e766da459bc716bf1f2f294990
Roberto Pereira DeDeus Jr <robertopereiradedeusjr@Johnny5.home>
2019-07-15 20:51:29 -0400

    Apply css and add event emitter to hero component.

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 import { HEROES } from '../mock-heros';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
 
+  selectedHero: Hero;
   heroes = HEROES;
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.heroes);
+  }
+
+  onSelect(hero: Hero):void {
+    this.selectedHero = hero;
   }
 
 }
